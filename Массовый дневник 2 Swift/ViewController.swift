dfce34ba9c90fe607bd66b7d3454550c92febc4a
Admin <admin@mac-admin.local>
2017-10-03 12:00:48 +0300

    Initial Commit

---
+++ Массовый дневник 2 Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Массовый дневник 2 Swift
+//
+//  Created by Admin on 03.10.17.
+//  Copyright © 2017 Bonaventura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
